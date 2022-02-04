a58e9e8630b2f61a326fb4ad674703a96780dae7
Alopmel <78558722+Alopmel@users.noreply.github.com>
2022-02-04 13:11:17 +0100

    Check if exist currentGame key in game

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -17,5 +17,7 @@
      test("score key exists", () => {
          expect("score" in game).toBe(true);
      });
-     
+     test("currentGame key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
  });
